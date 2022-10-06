3cf3eac7f485e869612868ae04e744dadf82f4d1
Lucas Justi <lucas.justiscariot@gmail.com>
2022-10-06 18:52:08 -0300

    new MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using UmLugar.Models;
+
+namespace UmLugar.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UmLugar.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
